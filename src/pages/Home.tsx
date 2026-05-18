d2e0420b3916a4e3e4eaa6014ce2bb9dd9df9090
jihoo <jihoop0620@gmail.com>
2026-05-19 08:55:07 +0900

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h2 className="post-title">Hello, I'm Jihoo</h2>
       <div className="post-container">
         <h3>i use nixos</h3>
-        <p>I am studying Haskell, Python, TypeScript, and React.</p>
+        <p>I am studying Haskell, Python, Rust, TypeScript, and React.</p>
       </div>
     </Layout>
   );
