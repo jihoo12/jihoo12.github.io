9284af5603640ae6469b53aba265c4d180218507
jihoo <jihoop0620@gmail.com>
2026-05-20 19:38:43 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./style.css";
 import Home from "./pages/Home";
 import Repo from "./pages/Repo";
 import Learn from "./pages/Learn";
-import Organizations from "./pages/Organizations";
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/repo" element={<Repo />} />
         <Route path="/learn" element={<Learn />} />
-        <Route path="/orgs" element={<Organizations />} />
       </Routes>
     </HashRouter>
   );
