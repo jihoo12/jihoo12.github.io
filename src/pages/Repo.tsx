4b39ab6932b17e4fde4feefa921f5cc10e29b761
jihoo <jihoop0620@gmail.com>
2026-05-20 19:32:11 +0900

    update

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -6,20 +6,20 @@ interface Repo {
 }
 
 const repos: Repo[] = [
-  { href: "https://github.com/jihoo12/ploty", label: "Ploty" },
-  { href: "https://github.com/jihoo12/tinyasm", label: "tinyasm" },
-  { href: "https://github.com/jihoo12/vii", label: "vii" },
-  { href: "https://github.com/jihoo12/rlogind", label: "rlogind" },
-  { href: "https://github.com/jihoo12/widget", label: "widget" },
-  { href: "https://github.com/jihoo12/oas", label: "oas" },
-  { href: "https://github.com/jihoo12/ros", label: "ros" },
+  { href: "https://github.com/jihoo12/ai-RAG", label: "ai RAG" },
+  { href: "https://github.com/jihoo12/haskell-blockchain", label: "haskell blockchain" },
   { href: "https://github.com/jihoo12/mylang", label: "mylang" },
+  { href: "https://github.com/jihoo12/oas", label: "oas" },
+  { href: "https://github.com/bug-free-octo-couscous/octo", label: "octo" },
+  { href: "https://github.com/jihoo12/ploty", label: "ploty" },
+  { href: "https://github.com/jihoo12/rlogind", label: "rlogind" },
+  { href: "https://github.com/jihoo12/ros", label: "ros" },
   { href: "https://github.com/jihoo12/rust-wgpu-2d-physics", label: "rust wgpu 2d physics" },
   { href: "https://github.com/jihoo12/SSG", label: "SSG" },
-  { href: "https://github.com/jihoo12/ai-RAG", label: "ai RAG" },
-  { href: "https://github.com/bug-free-octo-couscous/octo", label: "octo" },
-  { href: "https://github.com/jihoo12/haskell-blockchain", label: "haskell blockchain" },
-  { href: "https://github.com/jihoo12/vm", label: "vm"},
+  { href: "https://github.com/jihoo12/tinyasm", label: "tinyasm" },
+  { href: "https://github.com/jihoo12/vii", label: "vii" },
+  { href: "https://github.com/jihoo12/vm", label: "vm" },
+  { href: "https://github.com/jihoo12/widget", label: "widget" },
 ];
 
 export default function Repo() {
