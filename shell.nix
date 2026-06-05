9860f6876e2fb72818a2d469135fed1a28301244
jihoo <jihoop0620@gmail.com>
2026-06-06 08:32:06 +0900

    update

--- shell.nix
+++ shell.nix
@@ -2,13 +2,12 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.nodejs_24  
-    pkgs.nodePackages.npm 
+    pkgs.nodejs_24 
   ];
 
-  # 쉘이 시작될 때 실행될 스크립트 (선택 사항)
   shellHook = ''
     echo "nodejs shell.nix"
     node --version
+    npm --version
   '';
 }
