6ddb0411d55c9c94b4bdd36d222f7d0ab0ecff25
jihoo <jihoop0620@gmail.com>
2026-06-06 09:39:15 +0900

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import Layout from "../Layout";
+import hitakiChitose from "../assets/hitaki_chitose.jpg";
 
 export default function Home() {
   return (
     <Layout>
+      <div className="hero-image-card">
+        <img src={hitakiChitose} alt="hitaki chitose" className="hero-image" />
+      </div>
       <h2 className="post-title">Hello, I'm Jihoo</h2>
       <div className="post-container">
         <h3>i use nixos</h3>
